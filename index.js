97e06e3644e61bb22cb1be116ec1e4fa38c7be66
Henock-Lite <litehenock@gmail.com>
2024-02-22 14:33:22 +0300

    js-mouss

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
 const navigation = document.querySelector(".first-Part");
 const logo = document.querySelector(".logo");
+const title = document.querySelector(".title-product >h2");
+const titre = document.querySelector(".title-product >h3");
+const Mov = document.querySelectorAll(".cards-product");
+const panier = document.querySelector(".panier");
 
 window.addEventListener("scroll", () => {
   let scroll = (window.scrollY + innerHeight) / document.body.offsetHeight;
   console.log(scroll);
   if (scroll > 0.31) {
     navigation.style.height = "90px";
-    logo.style.opacity="1";
+    logo.style.opacity = "1";
   } else {
     navigation.style.height = "50px";
   }
   if (scroll > 0.17) {
-     premier.style.color = "#fc484b";
+    premier.style.color = "#fc484b";
   }
-  
-  if (scroll > 0.30) {
+  if (scroll > 0.3) {
     deuxieme.style.color = " #fc484b";
     premier.style.color = "#22244d";
   } else {
     deuxieme.style.color = " #22244d";
   }
-
-  if (scroll > 0.78) {
+  if (scroll > 0.73) {
     troisieme.style.color = " #fc484b";
     deuxieme.style.color = "#22244d";
+    title.style.transform = "translateX(0)";
   } else {
     troisieme.style.color = "#22244d";
   }
+
+  if (scroll > 0.84) {
+    titre.style.transform = "translateX(0)";
+  }
+});
+
+Mov.forEach((boxe) => {
+  boxe.addEventListener("mouseover", () => {
+    panier.style.opacity = "1";
+  });SSs
 });
